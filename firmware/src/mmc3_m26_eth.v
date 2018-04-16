7f8651fa838c76e97ecf6f3f802e8dbfd890923b
Jens Janssen <janssen@physik.uni-bonn.de>
2018-04-16 11:03:48 +0200

    ENH: in order to work with pyTLU increase trigger number bit size to 32bits

--- firmware/src/mmc3_m26_eth.v
+++ firmware/src/mmc3_m26_eth.v
@@ -493,7 +493,8 @@ tlu_controller #(
     .BASEADDR(TLU_BASEADDR),
     .HIGHADDR(TLU_HIGHADDR),
     .DIVISOR(8),
-    .ABUSWIDTH(32)
+    .ABUSWIDTH(32),
+    .TLU_TRIGGER_MAX_CLOCK_CYCLES(32)
 ) i_tlu_controller (
     .BUS_CLK(BUS_CLK),
     .BUS_RST(BUS_RST),
