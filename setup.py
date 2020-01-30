5ccb8751c6f6625588f7fe26e8e0280236fa17ba
Yannick Dieter <dieter@physik.uni-bonn.de>
2020-01-30 14:50:43 +0100

    WIP: add online monitor for pymosa

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
 
+import pymosa
+
 version = '0.0.1'
 author = 'Ivan Caicedo, Yannick Dieter, Toko Hirono, Jens Janssen, David-Leon Pohl'
 author_email = 'caicedo@physik.uni-bonn.de, dieter@physik.uni-bonn.de, hirono@physik.uni-bonn.de, janssen@physik.uni-bonn.de, pohl@physik.uni-bonn.de'
@@ -24,13 +26,31 @@ setup(
     maintainer_email=author_email,
     install_requires=install_requires,
     packages=find_packages(),
-    setup_requires=['setuptools'],
+    setup_requires=['setuptools', 'online_monitor>=0.4<0.5'],
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
     keywords=['silicon', 'detector', 'telescope', 'Mimosa26', 'EUDET'],
     platforms='any',
     entry_points={
         'console_scripts': [
-            'pymosa = pymosa.m26:main'
+            'pymosa = pymosa.m26:main',
+            'pymosa_monitor = pymosa.online_monitor.start_pymosa_online_monitor:main',
         ]
     },
 )
+
+# FIXME: bad practice to put code into setup.py
+# Add the online_monitor Pymosa plugins
+try:
+    import os
+    from online_monitor.utils import settings
+    # Get the absoulte path of this package
+    package_path = os.path.dirname(pymosa.__file__)
+    # Add online_monitor plugin folder to entity search paths
+    settings.add_producer_sim_path(os.path.join(package_path,
+                                                'online_monitor'))
+    settings.add_converter_path(os.path.join(package_path,
+                                             'online_monitor'))
+    settings.add_receiver_path(os.path.join(package_path,
+                                            'online_monitor'))
+except ImportError:
+    pass
