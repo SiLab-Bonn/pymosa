649dd7520df3d469628fb58908af01e2654b3553
rpartzsch <rasmus.partzsch@hotmail.com>
2026-03-25 09:49:29 +0100

    docs (constellation): added readme for satellite

---
+++ pymosa/constellation/READMDE.md
@@ -0,0 +1,80 @@
+---
+title: "Pymosa"
+description: "Satellite for controlling a EUDET-type Beam telescope using a MMC3 readout board"
+category: "Readout Systems"
+language: "Python"
+parent_class: "TransmitterSatellite"
+---
+
+## Description
+
+This satellite controls the readout of a EUDET-type beam telescope using a [MMC3 readout board](https://hdl.handle.net/20.500.11811/7265). 
+
+## Building
+
+Clone the repository:
+
+```sh
+git clone https://github.com/SiLab-Bonn/pymosa
+```
+
+Install via:
+```sh
+cd pymosa
+pip install ConstellationDAQ
+pip install -e .
+```
+
+## Usage
+
+Set the correct IP address in [pymosa/m26.yaml](https://github.com/SiLab-Bonn/pymosa) for more information.
+
+Start the satellite with:
+
+```sh
+python pymosa/constellation/__main__.py -g testbeam -n ANEMONE
+```
+
+## Parameters
+
+| Configuration | Description | Type | Default Value |
+|-----------|-------------|------| ------|
+| `no_data_timeout` | (Required) No data timeout after which the scan will be aborted, in seconds; if 0, the timeout is disabled. | String | None |
+| `send_data` | (Required) TCP address to which the telescope data is send; to allow incoming connections on all interfaces use 0.0.0.0 | String | None |
+| `max_triggers` | (Required) Maximum number of triggers; if 0, there is no limit on the number of triggers. | Integer | None |
+| `scan_timeout` | (Optional) Timeout after which the scan will be stopped, in seconds; if 0, the timeout is disabled. | Integer | 0 |
+| `run_number` | (Optional) Base run number, will be automatically increased; if none is given, generate filename | Integer | None |
+| `output_folder` | (Optional) Output folder for the telescope data; if none is given, the current working directory is used. | String | None |
+| `m26_configuration_file` | (Optional) Configuration file for Mimosa26 sensors, if note stated a default one is used. | String | 'm26_config/m26_threshold_8.yaml' |
+| `m26_jtag_configuration` | (Optional) Send Mimosa26 configuration via JTAG. | String | True |
+| `enabled_m26_channels` | (Optional) Enabled RX channels, eg. ["M26_RX1", "M26_RX2", "M26_RX6"]; default None (=all planes) | String | None |
+
+
+### Configuration Example
+
+An example configuration for the TJ-Monopix2 satellite which could be dropped into a Constellation configuration as a starting point:
+
+```toml
+[pymosa.ANEMONE]
+run_number = "None"  
+output_folder = "None"  
+m26_configuration_file = "None"  
+m26_jtag_configuration = true  
+no_data_timeout = 30 
+scan_timeout = 0 
+max_triggers = 0 
+send_data = 'tcp://127.0.0.1:8500'
+enabled_m26_channels = "None"
+```
+
+## Metrics
+
+The following metrics are distributed by this satellite and can be subscribed to. Timed metrics provide an interval in units of time, triggered metrics in number of calls.
+
+| Metric | Description | Value Type | Interval |
+|--------|-------------|------------|----------|
+| `TRIGGER_NUMBER` | Number of recieved triggers | Int | 1s |
+
+## Data
+
+Data is saved in HDF5 format in the `output_folder`.
---
+++ pymosa/constellation/pymosa_satellite.py
@@ -0,0 +1,127 @@
+from constellation.core.configuration import Configuration
+from constellation.core.satellite import Satellite
+import time
+from constellation.core.cmdp import MetricsType
+from constellation.core.fsm import SatelliteState
+from constellation.core.monitoring import schedule_metric
+import os
+import yaml
+import pymosa
+from pymosa.m26 import m26
+import logging
+from pymosa.m26_raw_data import save_configuration_dict
+from time import sleep, strftime, time
+from tqdm import tqdm
+from typing import Any
+
+class Pymosa(Satellite):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def do_initializing(self, config: Configuration) -> None:
+        # Open Mimosa26 std. configuration
+        pymosa_path = os.path.dirname(pymosa.__file__)
+        with open(os.path.join(pymosa_path, 'm26_configuration.yaml'), 'r') as f:
+            self.yaml_config = yaml.safe_load(f)
+        # overwrite some configuration variables with toml config
+        configuration = config.get_dict()
+        if configuration["run_number"] == "None":
+            configuration["run_number"] = None
+        if configuration["output_folder"] == "None":
+            configuration["output_folder"] = None
+        if configuration["m26_configuration_file"] == "None":
+            configuration["m26_configuration_file"] = None
+        if configuration["enabled_m26_channels"] == "None":
+            configuration["enabled_m26_channels"] = None
+        for key in configuration.keys():
+            self.yaml_config[key] = configuration[key]
+        # Create telescope object and load hardware configuration
+        self.telescope = m26(conf=None)  # None: use default hardware configuration
+        return "init done"
+
+    def do_launching(self):
+        # Initialize telescope hardware and set up parameters
+        self.telescope.init(init_conf=self.yaml_config)
+        return "launching done"
+
+    def do_run(self, payload=None) -> None:
+        self._pre_run()
+        with self.telescope.access_file():
+            save_configuration_dict(self.telescope.raw_data_file.h5_file, 'configuration', self.telescope.telescope_conf)
+            with self.telescope.readout(enabled_m26_channels=self.telescope.enabled_m26_channels):
+                got_data = False
+                start = time()
+                while not self.stop_scan and not self._state_thread_evt.is_set():
+                    sleep(1.0)
+                    if not got_data:
+                        if self.telescope.m26_readout.data_words_per_second()[0] > 0:
+                            got_data = True
+                            logging.info('Taking data...')
+                            if self.telescope.max_triggers:
+                                self.pbar = tqdm(total=self.telescope.max_triggers, ncols=80)
+                            else:
+                                self.pbar = tqdm(total=self.telescope.scan_timeout, ncols=80)
+                    else:
+                        triggers = self.telescope.dut['TLU']['TRIGGER_COUNTER']
+                        try:
+                            if self.telescope.max_triggers:
+                                self.pbar.update(triggers - self.pbar.n)
+                            else:
+                                self.pbar.update(time() - start - self.pbar.n)
+                        except ValueError:
+                            pass
+                        if self.telescope.max_triggers and triggers >= self.telescope.max_triggers:
+                            self.stop_scan = True
+                            self.pbar.close()
+                            logging.info('Trigger limit was reached: %i' % self.telescope.max_triggers)
+        logging.info('Total amount of triggers collected: %d', self.telescope.dut['TLU']['TRIGGER_COUNTER'])
+        self.logger.removeHandler(self.fh)
+        logging.info('Data Output Filename: %s', self.telescope.run_filename + '.h5')
+        self.telescope.analyze()
+        return "running done"
+
+    def do_landing(self):
+        # Close the resources
+        self.telescope.close()
+        return "landing done"
+
+    def _pre_run(self):
+        self.stop_scan = False
+        # signal.signal(signal.SIGINT, self.telescope._signal_handler)
+        logging.info('Press Ctrl-C to stop run')
+
+        # check for filename that is not in use
+        while True:
+            if not self.telescope.output_filename and self.telescope.run_number:
+                filename = 'run_' + str(self.telescope.run_number) + '_' + self.telescope.run_id
+
+            else:
+                if self.telescope.output_filename:
+                    filename = self.telescope.output_filename
+                else:
+                    filename = strftime("%Y%m%d-%H%M%S") + '_' + self.telescope.run_id
+            if filename in [os.path.splitext(f)[0] for f in os.listdir(self.telescope.working_dir) if os.path.isfile(os.path.join(self.telescope.working_dir, f))]:
+                if not self.telescope.output_filename and self.telescope.run_number:
+                    self.telescope.run_number += 1  # increase run number and try again
+                    continue
+                else:
+                    raise IOError("Filename %s already exists." % filename)
+            else:
+                self.telescope.run_filename = os.path.join(self.telescope.working_dir, filename)
+                break
+
+        # set up logger
+        self.fh = logging.FileHandler(self.telescope.run_filename + '.log')
+        self.fh.setLevel(logging.DEBUG)
+        FORMAT = '%(asctime)s [%(name)-17s] - %(levelname)-7s %(message)s'
+        self.fh.setFormatter(logging.Formatter(FORMAT))
+        self.logger = logging.getLogger()
+        self.logger.addHandler(self.fh)
+        self.telescope.dut['TLU']['TRIGGER_COUNTER'] = 0
+
+    @schedule_metric("", MetricsType.LAST_VALUE, 1)
+    def trigger_number(self) -> Any:
+        if self.fsm.current_state_value == SatelliteState.RUN:
+            return self.telescope.dut['TLU']['TRIGGER_COUNTER']
+        else:
+            return None
